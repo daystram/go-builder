6c431bc5fb7ed04dcc1b6de82674445114773226
Danny August Ramaputra <daystram@gmail.com>
2020-12-18 22:02:22 +1000

    Added job generator

--- generate-jobs.sh
+++ generate-jobs.sh
@@ -1,2 +1,41 @@
-#!/bin/sh
-ls
+#!/bin/bash
+echo "Generating configuration..."
+
+JOBS=""
+
+for VERSION in *; do
+    if [ -d "$VERSION" ]; then
+        echo "- go-builder:$VERSION"
+        read -r -d '' JOBS << EOM
+$JOBS
+
+go-builder:$VERSION:
+  stage: build
+  script:
+    - docker build --tag go-builder:$VERSION $VERSION/
+EOM
+    fi
+done
+
+cat << EOM > config.yml
+image: docker:19.03.12
+
+variables:
+  DOCKER_TLS_CERTDIR: "/certs"
+
+services:
+  - docker:19.03.12-dind
+
+stages:
+  - build
+  - push
+
+$JOBS
+
+deploy:
+  stage: push
+  script:
+    - echo "Pushing image..."
+EOM
+
+echo "Done!"
